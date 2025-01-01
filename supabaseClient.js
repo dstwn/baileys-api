bf00526b23dc6eacf5bfe8988e3d41a47d011b7c
dstwn <dwi.setiawan@bonofactum.com>
2025-01-02 04:51:41 +0800

    fix: add logging for Supabase client initialization

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,5 +4,5 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-
+console.log('supabase :', supabase);
 export default supabase;
